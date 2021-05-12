e330904be911211308405d881ce1aeb8220028fa
Spencer Hurd <spencerhurd@innovationsimple.com>
2021-05-12 11:54:39 -0600

    JSON

--- Python/WhenAndWhereToEat-v1-2021_03_26.py
+++ Python/WhenAndWhereToEat-v1-2021_03_26.py
@@ -4,7 +4,7 @@
 
 import random
 
-def whenAndWhereToEat():
+def whenAndWhereToEat_v1():
     locationChoices = ["Viva Chicken", "Angelica's", "Ernesto's"]
     location = random.randrange(len(locationChoices))
 
@@ -13,4 +13,44 @@ def whenAndWhereToEat():
 
     return ("Location: " + locationChoices[location] + ". " + "Leave at: " + timeChoices[time])
 
-print(whenAndWhereToEat())
+# print(whenAndWhereToEat_v1())
+
+
+
+# VERSION 2
+
+def whenAndWhereToEat_v2():
+    print("Welcome to the Lunch Decision Maker.")
+
+    print("Please enter the name of restaurants you want to add as choices. Please enter them one at a time and hit 'Enter' after each one. When you are finished, please type 'Done' to move to the next step.")
+    restInput = ""
+    restautrants = []
+    while restInput != 'Done' or restInput != 'done':
+        restInput = input("Please enter the name of a restaurant, then press 'Enter'. ")
+        restautrants.append(restInput)
+
+    earlyInput = input("Please enter the earliest time you would like to leave for lunch (24 hour format: e.g. 10:55): ")
+    startTime = ""
+    for char in earlyInput: 
+        if char != ":":
+            startTime.append(char)
+        continue
+    startTime = int(startTime)
+
+    lateInput = input("Please enter the lastest time you would be willing to leave for lunch (e.g. 14:00): ")
+    lateTime = ""
+    for char in lateInput: 
+        if char != ":":
+            lateTime.append(char)
+        continue
+    lateTime = int(lateTime)
+
+    chosenRestaurant = random.randrange(len(restautrants))
+
+    chosenTime = random.randrange(startTime, lateTime, 15)
+
+    return("Location: " + restautrants[chosenRestaurant] + ". Time: " + chosenTime)
+
+
+
+print(whenAndWhereToEat_v2())
